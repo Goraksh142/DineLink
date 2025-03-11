ca9f5a46ca5b1a6ae579c4c8529a77b07a9d6923
Goraksh_git <72146123+Goraksh142@users.noreply.github.com>
2025-03-11 17:40:55 +0530

    first commit

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order your <br></br>favourite food here</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
